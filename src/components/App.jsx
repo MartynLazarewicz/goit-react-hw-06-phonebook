e3767d4ca58d16818ff4015796689631f6d8eff4
Martyn Lazarewicz <mk.lazarewicz@gmail.com>
2022-10-12 17:21:45 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,52 @@
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
-import { AppWrapper, LogoIcon } from './App.styled';
+import { useEffect } from 'react';
+import { Contacts } from './Contacts';
+import { Filter } from './Filter';
+import { Title } from './Styles';
+import { PhoneBookForm } from './PhonebookForm';
+import { useDispatch, useSelector } from 'react-redux';
+import * as contactActions from 'redux/actions';
 
 export const App = () => {
+  const contactState = useSelector(state => state.contacts.items);
+  const contactFilter = useSelector(state => state.contacts.filter);
+  useSelector(state => console.log(state.contacts.items));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contactState));
+  }, [contactState]);
+
+  const handleChange = e => {
+    dispatch(contactActions.addFilter(e.target.value));
+  };
+
+  const filterByName = () => {
+    return contactState.filter(contact => {
+      return contact.name.toLowerCase().includes(contactFilter.toLowerCase());
+    });
+  };
+
+  const contactSubmit = values => {
+    const nameArray = contactState.map(contact => {
+      return contact.name;
+    });
+    if (nameArray.includes(values.name)) {
+      return alert(`${values.name} is already in contacts.`);
+    }
+    return dispatch(contactActions.addContact(values));
+  };
+
+  const toDelete = id => {
+    dispatch(contactActions.deleteContact(id));
+  };
+
   return (
-    <AppWrapper>
-      <h1>
-        <LogoIcon />
-        Phonebook
-      </h1>
-      <ContactForm />
-      <h2>Contacts</h2>
-      <Filter />
-      <ContactList />
-    </AppWrapper>
+    <div>
+      <h1>Phonebook</h1>
+      <PhoneBookForm onSubmit={contactSubmit} />
+      <Title>Contacts</Title>
+      <Filter value={contactFilter} filterByName={handleChange} />
+      <Contacts filterByName={filterByName} toDelete={toDelete} />
+    </div>
   );
 };
-
-export default App;
