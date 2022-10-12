39b1acc4e2200c47861d927a563d0c3e4e02b92d
Martyn Lazarewicz <mk.lazarewicz@gmail.com>
2022-10-12 17:31:08 +0200

    Update items.js

--- src/redux/items.js
+++ src/redux/items.js
@@ -1,6 +1,6 @@
 export const items = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-1', name: 'Simson Simson', number: '333-23-14' },
+  { id: 'id-2', name: 'Albert Maly', number: '412-55-76' },
+  { id: 'id-3', name: 'Sandra Mus', number: '666-98-99' },
+  { id: 'id-4', name: 'Beata Cosik', number: '545-54-44' },
 ];
